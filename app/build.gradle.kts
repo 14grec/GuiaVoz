272740b4c15e66c9f1094b7e06a9656b8f4cabac
14grec <vieirablog16@gmail.com>
2026-08-14 18:40:38 -0300

    Melhora chamadas do WhatsApp na versão 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "br.com.exemplo.guiavoz"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "android.app.Instrumentation"
     }
